cf17b3104d18c0e3a7019ce90e2a9b65c902343a
tianyingchun <tianyingchun@outlook.com>
2024-08-01 20:19:31 +0800

    bump

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Button } from "@repo/ui/button";
+import { Button } from "@repo/ui";
 import styles from "./page.module.css";
 
 export default function Home() {
